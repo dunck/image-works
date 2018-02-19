731014e8e37a796c2ab3637eb4aa74965d0929ca
Duncan Kirkland <duncanck0@gmail.com>
2018-02-19 19:50:45 +1000

    Structure definitions.

---
+++ configuration.ps1
@@ -0,0 +1,43 @@
+<# Duncan Kirkland
+<# 19/02/18
+<# configuration.ps1
+<#>
+
+$XmlConfiguration = [xml](Get-Content ".\configuration.xml") | Select -ExpandProperty "Configuration"
+
+$VMSettings =
+@{
+    "General" =
+    @{
+        "NewVHDPath"         = $XmlConfiguration.Environment.VHDXPath
+        "BootDevice"         = "CD"
+        "NewVHDSizeBytes"    = 64GB  # note: this will create dynamically expanding disk
+        "MemoryStartupBytes" = 4GB   # no more than 4GB is necessary when in WinPE
+        "Generation"         = 1
+        "SwitchName"         = $XmlConfiguration.Network.HyperVSwitchName # consider using an internal-only switch
+    }
+
+    "Processor" =
+    @{
+        "Count"          = 4
+        "Reserve"        = 10
+        "Maximum"        = 75
+        "RelativeWeight" = 200 # modify this to your liking
+    }
+
+    "NetworkAdapter" =
+    @{
+        "StaticMacAddress" = $XmlConfiguration.Network.ReservedStaticIP # this needs to be an IP that can reach Microsoft Update, if not using WSUS/similar
+    }
+
+    "DVDDrive" =
+    @{
+        "Path"   = $XmlConfiguration.Environment.DeploymentShareUNC + "\Boot\LiteTouchPE_x64.iso"
+    }
+
+    "BIOS" =
+    @{
+        "StartupOrder" = "IDE","CD","Floppy","LegacyNetworkAdapter"
+    }
+
+}
---
+++ imagefactory.ps1
@@ -0,0 +1,32 @@
+<# Duncan Kirkland
+<# 16/04/17
+<# imagefactory.ps1
+<#>
+
+Function New-IFVM($VMName)
+{
+
+}
+
+Function Set-IFVM($VMName)
+{
+
+}
+
+Function Start-IFVM($VMName)
+{
+
+}
+
+Function Assert-IFEnvironment()
+{
+
+}
+
+Assert-IFEnvironment
+
+New-IFVM "ImageFactory01"
+
+Set-IFVM "ImageFactory01"
+
+Start-IFVM "ImageFactory01"
